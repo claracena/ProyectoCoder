c895f0493a1eade363f183346a0dd3f6a0bf64a6
Cesar Aracena <claracena@users.noreply.github.com>
2023-02-08 19:09:30 -0300

    Clase 20 completa

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,6 +6,9 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
+    def __str__(self):
+        return self.nombre + ' (' + str(self.camada) + ')'
+
 class Estudiante(models.Model):
 
     nombre = models.CharField(max_length=30)
@@ -18,6 +21,9 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = 'Profesores'
 
 class Entregable(models.Model):
     
